aa7f4dcd24fca40ced7e8e0c02e0a9135f092604
Brutski93 <abrutski@gmail.com>
2025-08-17 18:54:35 +0300

    feat: 100%

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -639,8 +639,15 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  array.map((item) => {
+    if (!map.has(keySelector(item))) map.set(keySelector(item), []);
+    if (map.has(keySelector(item)))
+      map.get(keySelector(item)).push(valueSelector(item));
+    return item;
+  });
+  return map;
 }
 
 /**
